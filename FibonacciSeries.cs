1ff5ade5fe8f2a390b98bdfecd62202e23c29ed6
rahul unni <rahul@rahulsmac.bbrouter>
2022-05-06 12:49:39 +0530

    Program added

---
+++ FibonacciSeries.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Day6Assignment
+{
+    public class FibonacciSeries
+    {
+        public FibonacciSeries()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day6Assignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day 6 Assignments Problems");
+            Console.WriteLine("\nProblem 1 : Fiboacci");
+            Console.WriteLine("Enter your choice : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            //Use switch case to run any particular program which user wants
+            switch (a)
+            {
+                case 1:
+                    FibonacciSeries series = new FibonacciSeries();
+                    series.Fibonacci();
+                    break;
+            }
+        }
+    }
+}
