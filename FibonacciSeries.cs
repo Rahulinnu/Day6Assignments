b4c02aef505ddb6a1a7ffb8f49faef6b9c181322
rahul unni <rahul@rahulsmac.bbrouter>
2022-05-06 12:52:16 +0530

    Fibonacci added

--- FibonacciSeries.cs
+++ FibonacciSeries.cs
@@ -3,8 +3,20 @@ namespace Day6Assignment
 {
     public class FibonacciSeries
     {
-        public FibonacciSeries()
+        public void Fibonacci()
         {
+            int firstNumber = 0, secondNumber = 1, sum = 0;
+            Console.WriteLine("\nEnter the number for fibonacci series : ");
+            int check = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nFibonacci Series is : ");
+            Console.Write($" {firstNumber} {secondNumber} ");
+            for (int i = 2; i < check; i++)
+            {
+                sum = firstNumber + secondNumber;
+                Console.Write($"{sum} ");
+                firstNumber = secondNumber;
+                secondNumber = sum;
+            }
         }
     }
 }
